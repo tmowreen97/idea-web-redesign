2ac27a44f1e5c1f5547e58b6c05d15d3c5b911ef
Tasreen Mowreen <tmowreen97@gmail.com>
2024-01-09 16:12:12 -0500

    updated intro page for mobile

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -4,21 +4,21 @@ import Image from 'next/image';
 function Intro(){
   return(
     <div className='w-screen h-full bg-primary font-thin text-primary_text tracking-widest '>
-      <div className="py-10">
-        <div className="p-20">
-        <div className="flex items-center justify-evenly">
-          <div >
-            <h1 className="text-5xl leading-[60px]">Now providing World Class Inter-Disciplinary Engineering and Construction services to collaborating partners in Bangladesh.</h1>
+      <div className="py-10 justify-center items-center">
+        <div className="xl:p-20 p-10">
+          <div className="xl:flex items-center justify-center">
+            <div>
+              <h1 className="xl:text-5xl xl:leading-[60px] text-2xl">Now providing World Class Inter-Disciplinary Engineering and Construction services to collaborating partners in Bangladesh.</h1>
+            </div>
+            <div className="items-center flex justify-center">
+              <Image src={'/assets/logos/Mechanicaltransparent cropped.gif'} width={2000} height={100}/>
+            </div>
           </div>
-          <div className="m-5">
-            <Image src={'/assets/logos/Mechanicaltransparent cropped.gif'} width={2000} height={100}/>
-          </div>
+      </div>
+      <div className="xl:px-20 px-10 py-10">
+        <div className="flex justify-right">
+          <h1 className="xl:text-7xl text-2xl ">Introducing <span className="text-red_accent font-semibold xl:text-8xl text-3xl tracking-tight">IDEA csc</span> (US IDEA CSC (PVT.) LTD.), a Sister Company of IDEA aec (USA).</h1>
         </div>
-      </div>
-      <div className="px-20 py-10">
-        <div className="flex items-center justify-center align-middle">
-        <h1 className="text-7xl ">Introducing <span class="text-red_accent font-semibold text-8xl tracking-tight">IDEA csc</span> (US IDEA CSC (PVT.) LTD.), A Sister Company of IDEA aec (USA).</h1>
-      </div>
       </div>
       </div>
       
