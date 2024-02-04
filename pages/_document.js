84d8f0eb8ca7c26f1bba0016fa5ac2d2874a5eee
Tasreen Mowreen <tmowreen97@gmail.com>
2024-02-04 15:01:51 -0500

    updated icon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link className='width-5 height-5'rel="icon" href="/assets/logos/idea-csc-symbol.png"  />
+      </Head>
       <body>
         <Main />
         <NextScript />
