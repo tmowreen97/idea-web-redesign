a65cc51de30fca4f3d1fba80c1c04d702c744625
Tasreen Mowreen <tmowreen97@gmail.com>
2024-07-23 15:05:34 -0400

    update footer social media icons to be light text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -98,7 +98,7 @@ export default function Footer({logo}){
             </div>
           <div className="grid grid-cols-3 gap-6 text-center items-center justify-center">
             <div className="text-center items-center justify-center flex rounded-xl ">
-              <div className="bg-button_bg text-dark_text hover:bg-dark_green hover:text-light_text ease-in-out duration-300 rounded-full p-4 text-2xl">
+              <div className="bg-button_bg hover:bg-dark_green text-light_text ease-in-out duration-300 rounded-full p-4 text-2xl">
                 <Link href="https://www.instagram.com/idea.aec/" legacyBehavior>
                     <a target="_blank" rel="noopener noreferrer" aria-label="Instagram">
                       <AiFillInstagram/>
@@ -107,7 +107,7 @@ export default function Footer({logo}){
               </div>
             </div>
             <div className="text-center items-center justify-center flex  p-1 rounded-xl ">
-              <div className="bg-button_bg text-dark_text hover:bg-dark_green hover:text-light_text ease-in-out duration-300 rounded-full p-4 text-2xl">
+              <div className="bg-button_bg hover:bg-dark_green text-light_text ease-in-out duration-300 rounded-full p-4 text-2xl">
                 <Link href="https://www.facebook.com/profile.php?id=100076951517206" legacyBehavior>
                     <a target="_blank" rel="noopener noreferrer" aria-label="Facebook">
                       <AiFillFacebook/>
@@ -116,7 +116,7 @@ export default function Footer({logo}){
               </div>
             </div>
             <div className="text-center items-center justify-center flex  p-1 rounded-xl ">
-              <div className="bg-button_bg text-dark_text hover:bg-dark_green hover:text-light_text ease-in-out duration-300 rounded-full p-4 text-2xl">
+              <div className="bg-button_bg hover:bg-dark_green text-light_text ease-in-out duration-300 rounded-full p-4 text-2xl">
                 <Link href="https://www.linkedin.com/company/idea-aec/" legacyBehavior>
                     <a target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
                       <AiFillLinkedin/>
