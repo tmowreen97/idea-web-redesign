76c4d89ee935da2d75d8d2a976fb37ba2e450091
Tasreen Mowreen <tmowreen97@gmail.com>
2024-09-16 14:46:09 -0400

    added plausible information

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <script defer data-domain="ideacsc.com" src="https://plausible.io/js/script.js"></script>
         <meta
             name="description"
             content="IDEA CSC (Construction Solutions Company), a sister company to IDEA AEC, provides Inter-Disciplinary Construction and Engineering services in Bangladesh."
